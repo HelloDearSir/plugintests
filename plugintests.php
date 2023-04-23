eed116652099513eb812dbedb31c038a3acf357c
HelloDearSir <jonbarton@hotmail.co.uk>
2023-04-23 16:03:01 +0100

    spelling

--- plugintests.php
+++ plugintests.php
@@ -8,7 +8,7 @@ Author: Test
 
 function testing() {
 wp_register_style( 'testingcss', plugins_url( 'plugintests/assets/css/main.css' ) ); 
-wp_equeue_style('testingcss');
+wp_enqueue_style('testingcss');
 }
 
 add_action('wp_enqueue_scripts','testing');
