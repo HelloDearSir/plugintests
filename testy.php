5ba074b0258f602a21e169c3ee4d8aba126705f0
HelloDearSir <jonbarton@hotmail.co.uk>
2023-04-23 15:24:54 +0100

    testing css

---
+++ testy.php
@@ -0,0 +1,16 @@
+<?php 
+
+/* 
+Plugin Name: MyPlugin
+Author: Test
+*/ 
+ 
+
+function testing() {
+wp_register_style( 'testingcss', plugins_url( 'plugintests/assets/css/main.css' ) ); 
+wp_equeue_style('testingcss');
+}
+
+add_action('wp_scripts','testing');
+
+?> 
